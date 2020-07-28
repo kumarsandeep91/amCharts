410730dc1579e2921320eed92d6ff44384fd76ab
Sandeep Sihari <kumarsandeep91@gmail.com>
2020-07-28 16:22:16 +0530

    redux debugger config added

--- src/index.js
+++ src/index.js
@@ -24,7 +24,10 @@ const Root = ({ store }) => (
   </Provider>
 );
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const engine = new Styletron();
 
